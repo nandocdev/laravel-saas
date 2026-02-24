704e09738cb7514cdd8c7f1b5f39b26da8cf13fd
Fernando Castillo <fdocst@gmail.com>
2026-02-23 23:06:21 -0500

    fix(tenancy): enable universal routes and register universal middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function () {
             Route::middleware('web')
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'universal' => \Stancl\Tenancy\Middleware\ScopeSessions::class,
+        ]);
+
         $middleware->redirectGuestsTo(function (\Illuminate\Http\Request $request) {
             if (function_exists('tenancy') && tenancy()->initialized) {
                 return route('tenant.login');
